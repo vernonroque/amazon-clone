0d5b34176e44c1c05c58fbbe9c96f2679e4fe8f3
vernonroque <vroque88@gmail.com>
2023-10-01 15:35:05 -0500

    Deployed backend to google cloud functions

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/clone-bfd8a/us-central1/api' //The API (cloud function) URL
+    baseURL: 'https://us-central1-clone-bfd8a.cloudfunctions.net/api' //The API (cloud function) URL
 });
 
+//https://us-central1-clone-bfd8a.cloudfunctions.net/api
+//http://127.0.0.1:5001/clone-bfd8a/us-central1/api    <<<<<< Use this for debugging purposes
 export default instance;
